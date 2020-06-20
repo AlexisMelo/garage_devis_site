aad1d19cb6c4629a20fbddf3eae782ee206b1348
Alexis Melo da silva <alexis.melo_da_silva@insa-rouen.fr>
2020-06-20 11:59:08 +0200

    add(base): ajout materialize et barre navigation

--- devis/forms.py
+++ devis/forms.py
@@ -1,4 +1,6 @@
 from django import forms
+from django.forms import DateField, DateInput, TextInput
+
 from .models import Devis, Prestation
 
 
@@ -11,6 +13,9 @@ class DevisAjoutForm(forms.ModelForm):
     class Meta:
         model = Devis
         fields = '__all__'
+        widgets = {
+            'date_planification': DateInput(attrs={'format' : 'dd-mm-yyyy','class': 'datepicker'})
+        }
 
 class DevisModifForm(forms.ModelForm):
     class Meta:
