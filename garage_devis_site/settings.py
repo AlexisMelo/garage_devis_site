08e4132ceb652807b4e98aced80d29f403c41d76
Alexis Melo da silva <alexis.melo_da_silva@insa-rouen.fr>
2020-06-22 00:00:24 +0200

    fix(tout): c bon ça marche mtn

--- garage_devis_site/settings.py
+++ garage_devis_site/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'a$kd^(^s1@_u^+f$15r$uzjq)()4)7e@jc70(jh=is13n!a(sm'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['dev-devis.tk','http://r0b0tiik.alwaysdata.net/','r0b0tiik.alwaysdata.net']
+ALLOWED_HOSTS = []
 
 
 # Application definition
@@ -127,7 +127,5 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, "static"),
 )
-
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 LOGIN_URL = "/utilisateurs/connexion"
