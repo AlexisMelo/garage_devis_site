d0f5d2029aa86d632eff5cf18af5430e9f69be96
Alexis Melo da silva <alexis.melo_da_silva@insa-rouen.fr>
2020-06-22 12:03:25 +0200

    -

--- garage_devis_site/settings.py
+++ garage_devis_site/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'a$kd^(^s1@_u^+f$15r$uzjq)()4)7e@jc70(jh=is13n!a(sm'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost','http://r0b0tiik.alwaysdata.net/']
 
 
 # Application definition
